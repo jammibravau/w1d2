9f19d84b6f86936b4acb79ae672bf6111d005a78
Michael J. Martins-Baptista <mjmartinsbaptista@gmail.com>
2018-07-03 10:12:18 -0400

    complete exercise3 age calculator

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,8 @@
 puts "What is your name?"
+name = gets.chomp.capitalize
+puts "Hi #{name}!"
+puts "#{name} what year were you born?"
+year_born = gets.chomp.to_i
+puts "what year is it?"
+current_year = gets.chomp.to_i
+puts "You are currently #{current_year - year_born} years old"
